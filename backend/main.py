22eef6761ccef5a4bfe94d36f487ac94a310b75b
Le Minh Hoang <lemihoang2004@gmail.com>
2026-06-08 11:16:11 +0700

    Fix env

--- backend/main.py
+++ backend/main.py
@@ -46,5 +46,5 @@ app.register_blueprint(notification_blueprint)
 
 socketio.init_app(app)
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)
 
